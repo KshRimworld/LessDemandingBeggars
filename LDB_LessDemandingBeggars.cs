65419121b9eb525defbe57b0bda27926116d1949
Ksh1042 <roman14@naver.com>
2024-05-04 17:09:00 +0900

    초기 커밋.

---
+++ LDB_LessDemandingBeggars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace LDB
+{
+    internal class LDB_LessDemandingBeggars : Mod
+    {
+        private const string SETTINGS_CATEGORY_TITLE = "Less Demanding Beggars";
+
+        public static LDB_Settings settings;
+
+        public LDB_LessDemandingBeggars(ModContentPack content) : base(content)
+        {
+            LDB_LessDemandingBeggars.settings = GetSettings<LDB_Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            LDB_LessDemandingBeggars.settings.DoSettingsWindowContents(inRect);
+            // base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return SETTINGS_CATEGORY_TITLE;
+        }
+    }
+}
---
+++ LDB_Settings.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+using RimWorld.QuestGen;
+using System;
+using UnityEngine;
+using Verse;
+
+namespace LDB
+{
+    internal class LDB_Settings : ModSettings
+    {
+        public static float BeggarRequestValueFactor = 0.85f;
+
+        public static float MaxRequestValue = 700f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref BeggarRequestValueFactor, "BeggarRequestValueFactor");
+            Scribe_Values.Look(ref MaxRequestValue, "MaxRequestValue");
+        }
+
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+
+            listingStandard.Begin(inRect);
+
+            BeggarRequestValueFactor = (float)Math.Round(listingStandard.SliderLabeled("LDB.BeggarRequestValueFactor".Translate() + "(" + BeggarRequestValueFactor.ToString() + ")", BeggarRequestValueFactor, 0.01f, 1.7f), 2);
+
+            listingStandard.Label(" ");
+
+            MaxRequestValue = (float)Math.Round(listingStandard.SliderLabeled("LDB.MaxRequestValue".Translate() + "(" + MaxRequestValue.ToString() + ")", MaxRequestValue, 300f, 1400f), 0);
+
+            listingStandard.Label(" ");
+
+            if (Widgets.ButtonText(new Rect(0f, listingStandard.CurHeight, 150f, 30f), "reset", true, true, true))
+            {
+                BeggarRequestValueFactor = 0.85f;
+                MaxRequestValue = 700f;
+            }
+
+
+            listingStandard.End();
+        }
+    }
+}
